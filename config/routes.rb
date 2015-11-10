d3587c89d712d5e35715fb2c4ed389d562b082d7
Val Butt <vfbutt@yahoo.com>
2015-11-10 19:13:57 -0400

    Tagging feature completed

--- config/routes.rb
+++ config/routes.rb
@@ -18,6 +18,7 @@ Rails.application.routes.draw do
       resources :comments
     end
 
+    resources :tags
   # Example resource route with options:
   #   resources :products do
   #     member do
